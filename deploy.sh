9163d2aab4acab528b52634a7348b08a0e29bbc1
youssifsayed <sayedgyussif@gmail.com>
2016-04-24 00:47:57 +0200

    Travis CI support 3 :(

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
-#!/bin/env bash
+#!bash
 
 git init
 
 git config user.name  "youssifsayed"
 git config user.email "sayedgyussif@gmail.com"
 
-git fetch https://github.com/youssifsayed/youssifsayed.github.io.git ./
+git fetch --branch=gh-pages https://github.com/youssifsayed/youssifsayed.github.io.git ./
 git remote add up "https:/$GITTOKEN@github.com/youssifsayed/youssifsayed.github.io.git"
 
-ls
 echo youssif.me>CNAME
 
 git add -A .
 git commit -a -m "New deployment!"
+
+ls
 
 git push -q up HEAD:gh-pages
