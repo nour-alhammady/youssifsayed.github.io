0cb2b11986738f34d5d63c48b5b3e1c2830b8ec9
youssifsayed <sayedgyussif@gmail.com>
2016-04-24 00:56:02 +0200

    git fetch to git clone

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
+#!bash
+
 git init
 
 git config user.name  "youssifsayed"
 git config user.email "sayedgyussif@gmail.com"
 
-git fetch --branch=gh-pages https://github.com/youssifsayed/youssifsayed.github.io.git ./
+git clone --branch=gh-pages https://github.com/youssifsayed/youssifsayed.github.io.git ./
 git remote add up "https:/$GITTOKEN@github.com/youssifsayed/youssifsayed.github.io.git"
 
 echo youssif.me>CNAME
