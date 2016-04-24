c8c53ad60fc06f5c2454849f147af6a92864031d
youssifsayed <sayedgyussif@gmail.com>
2016-04-24 02:25:43 +0200

    deploy.sh update

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,12 @@ git config user.name  "youssifsayed"
 git config user.email "sayedgyussif@gmail.com"
 
 git remote add up "https://$GITTOKEN@github.com/youssifsayed/youssifsayed.github.io.git"
+git fetch upstream && git reset upstream/gh-pages
+
+git rm -rf .
+git add -A .
 
 echo youssif.me>CNAME
-
-git add -A .
 git commit -a -m "New deployment!"
 
 ls
